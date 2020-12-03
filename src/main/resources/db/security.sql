d3572590feb28f8063c3823a3191149a840e8e6d
Admin <mihailbichkov@yandex.ru>
2020-12-03 21:24:42 +0500

    configuring authentication data

--- src/main/resources/db/security.sql
+++ src/main/resources/db/security.sql
@@ -5,6 +5,6 @@ DELETE FROM authorities;
 insert into authorities (authority) values ('ROLE_USER');
 insert into authorities (authority) values ('ROLE_ADMIN');
 
-insert into users (username, password, authority_id)
-values ('root', '$2a$10$4Ucq.Qc9AwHdOsLjnwI7ZeLyifBTnCsrK3YETVtgYLPXR1Q0weglO',
+insert into users (username, password, enabled, authority_id)
+values ('root', '$2a$10$4Ucq.Qc9AwHdOsLjnwI7ZeLyifBTnCsrK3YETVtgYLPXR1Q0weglO', true,
         (select id from authorities where authority = 'ROLE_ADMIN'));
